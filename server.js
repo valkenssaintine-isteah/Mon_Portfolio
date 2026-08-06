ada02c5cce0ddf581240a4c23dd77dcf2f5051b9
Pharah Pierre-Jules <isteah.ppierrejules@gmail.com>
2026-08-06 14:54:04 -0400

    Fix: Express static folder fallback and host binding

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const messagesFile = path.join(__dirname, 'messages.json');
 
 if (!fs.existsSync(messagesFile)) {
